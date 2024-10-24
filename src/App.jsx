045b98a0d6de617ebd7ca12e220fd245f4660180
Joao Espindola <joaovitorfespindola@gmail.com>
2024-10-23 21:40:03 -0300

    fix login and home

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import Login from './components/pages/Login/Login'; 
 import './App.css';
-
+import NavigationBar from './components/pages/NavigationBar/NavigationBar';
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
     <Router>
+      <div className='App'>
       <Routes>
-        <Route path='/' element={<Login />} />
+        <Route path='/Login' element={<Login />} />
+        <Route path= '/NavigationBar' element = {<NavigationBar/>}></Route>
       </Routes>
+      </div>
     </Router>
   );
 }
