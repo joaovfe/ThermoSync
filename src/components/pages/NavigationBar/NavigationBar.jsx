3ab298913d106d8b669c9ed63e669cda7ebfd4ae
Arthur Ghizi <arthur.253815@alunosatc.edu.br>
2024-11-06 20:56:57 -0300

    fix components to sidebar

--- src/components/pages/NavigationBar/NavigationBar.jsx
+++ src/components/pages/NavigationBar/NavigationBar.jsx
@@ -1,53 +1,49 @@
+import React, { useState } from 'react';
 import './NavigationBar.css';
 import Dashboard from '../Dashboard/Dashboard';
+import Sidebar from './SideBar';
 
-function Home(){
+function Home() {
+    const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+    const toggleSidebar = () => {
+        setSidebarOpen(!isSidebarOpen);
+    };
+
     return (
         <div>
-            <NavigationBar />
+            <NavigationBar toggleSidebar={toggleSidebar} />
+            <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
             <Dashboard />
         </div>
-    )
+    );
 }
-function NavigationBar() {
-  return (
-    <header className="navbar">
-      <div className="container">
-        <a href="/#" className="navbar-brand">Dashboard</a>
-        {/* <nav className="navbar-menu">
-          <ul className="navbar-list">
-            <li className="navbar-item">
-              <a href="#header" className="navbar-link"></a>
-            </li>
-            <li className="navbar-item">
-              <a href="#about" className="navbar-link"></a>
-            </li>
-            <li className="navbar-item">
-              <a href="#projects" className="navbar-link"></a>
-            </li>
-            <li className="navbar-item">
-              <a href="#contact" className="navbar-link"></a>
-            </li>
-          </ul>
-        </nav> */}
-        <div className="navbar-info">
-          <div className="navbar-info-item">
-            <h6>Tempo sem pausas:</h6>
-            <h3>13 dias</h3>
-            </div>
-          <div className="navbar-info-item">
-            <h6>Média de Temperatura</h6>
-            <h3>-4°C</h3>
-            </div>
-          <div className="navbar-info-item">
-            <h6>Freezers online: </h6>
-            <h3>20/20</h3>
-            </div>
-        </div>
 
-      </div>
-    </header>
-  );
+function NavigationBar({ toggleSidebar }) {
+    return (
+        <header className="navbar">
+            <div className="container">
+                <a href="/#" className="navbar-brand">Dashboard</a>
+                <div className="navbar-info">
+                    <div className="navbar-info-item">
+                        <h6>Tempo sem pausas:</h6>
+                        <h3>13 dias</h3>
+                    </div>
+                    <div className="navbar-info-item">
+                        <h6>Média de Temperatura</h6>
+                        <h3>-4°C</h3>
+                    </div>
+                    <div className="navbar-info-item">
+                        <h6>Freezers online:</h6>
+                        <h3>20/20</h3>
+                    </div>
+                </div>
+              <button onClick={toggleSidebar} className="menu-btn">
+                  &#9776; Menu
+              </button>
+            </div>
+        </header>
+    );
 }
 
 export default Home;
---
+++ src/components/pages/NavigationBar/SideBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './SideBar.css';
+
+function Sidebar({ isOpen, toggleSidebar }) {
+    return (
+        <div className={`sidebar ${isOpen ? "open" : ""}`}>
+            <button onClick={toggleSidebar} className="close-btn">×</button>
+            <nav>
+                <ul>
+                    <li><a href="#section1">Section 1</a></li>
+                    <li><a href="#section2">Section 2</a></li>
+                    <li><a href="#section3">Section 3</a></li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default Sidebar;
